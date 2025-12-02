2057a05010dde7e2709fcf141c7a0bef5485340b
NadyKoskei <cherotichnady@gmail.com>
2025-12-02 13:16:36 +0300

    exercise 1

---
+++ variables.js
@@ -0,0 +1,12 @@
+//VARIABLES are used to store data values that can be referenced and manipulated in a program.
+
+let age; //declaration
+age = 20; //assignment
+
+const Name = "Nady"; //INITIALIZATION is declaration and assignment at the same time
+
+console.log("i am " + age + " years old.");
+//CONCATENATION is using the + operator to combine strings and variables together.
+
+age = age + 10; //REASSIGNMENT is updating the variable age by adding 10 to its current value
+console.log("in 10 years, I will be " + age + " years old");
