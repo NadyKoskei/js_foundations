d8fb306a9d01ba758f9bd686020d780782a443b3
NadyKoskei <cherotichnady@gmail.com>
2025-12-16 11:14:57 +0300

    question3

--- exercise.js
+++ exercise.js
@@ -14,6 +14,17 @@ function sum(a, b) {
 let sumResult = sum(17, 62); //calling the sum function with 17 and 62 as arguments
 console.log("Sum:", sumResult); //output the result
 
-console.log("Sum:", sum(10, 5)); //directly calling the sum function inside console.log
+console.log("Sum:", sum(11, 35)); //directly calling the sum function inside console.log
 
 //3.writting a function that checks if a number is even or odd and returns a corresponding message.
+function checkEvenOdd(num) {
+if (num % 2 === 0) {
+  return num + " is an even number.";}
+  else {
+    return num + " is an odd number.";
+  }
+}
+console.log (checkEvenOdd(10)); //calling the function with 10 as argument
+console.log (checkEvenOdd(7)); //calling the function with 7 as argument
+
+//4.
