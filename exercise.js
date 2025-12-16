410a68a5b39962ff1f6d74a97668c13124e415d7
NadyKoskei <cherotichnady@gmail.com>
2025-12-16 11:00:50 +0300

    question2

--- exercise.js
+++ exercise.js
@@ -1,9 +1,19 @@
 //1.writting a greet function that takes a name as parameter and prints a greeting message to the console.
 function greet(name) {
-    console.log("hello " + name + "!");
+  console.log("hello " + name + "!");
 }
 
 greet("Nady");
 
 //2.writting a function that calculates the sum of two numbers and returns the result.
 
+function sum(a, b) {
+  return a + b;
+}
+//return is used to output the result of the function when called.
+let sumResult = sum(17, 62); //calling the sum function with 17 and 62 as arguments
+console.log("Sum:", sumResult); //output the result
+
+console.log("Sum:", sum(10, 5)); //directly calling the sum function inside console.log
+
+//3.writting a function that checks if a number is even or odd and returns a corresponding message.
